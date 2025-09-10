5bb93796882343ce1b34d9589010f413531b8997
sanchitacodes <sanchitar2003@gmail.com>
2025-09-11 02:59:00 +0530

    done1

---
+++ BubbleSort.java
@@ -0,0 +1,18 @@
+public class BubbleSort {
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) break; // optimization
+        }
+    }
+}
+
+// Time: Best O(n), Avg O(n²), Worst O(n²)
---
+++ SelectionSort.java
@@ -0,0 +1,17 @@
+public class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+}
+
+// Time: Best O(n²), Avg O(n²), Worst O(n²)
